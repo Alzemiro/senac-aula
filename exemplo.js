194b851b26913ebfa59ac85bb34cd8e29d1693fa
Alzemiro Iago de Moraes Thomaz <iagomoraest@gmail.com>
2023-08-24 21:10:24 -0300

    Commit Inicial

---
+++ exemplo.js
@@ -0,0 +1,11 @@
+const senha = 'senhasegura1234';
+
+if(senha == 'senhasegura12345'){
+
+	console.log('Acesso liberado');
+
+}else{
+
+    console.log('Acesso negado');
+
+}
---
+++ exercicio.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+let idade = +prompt("Insira sua idade: ");
+
+// Desenvolva abaixo uma solução que respeite a seguintes regras:
+
+/* - Se a idade for maior que 18, exiba no console com o comando console.log('Pode beber álcool'), 
+senão exiba, com o mesmo comando a mensagem 'Não Pode beber álcool';
+*/
