ba993eecc99e2a32eff2939bd31e8efcd6bafb4c
A. Iago Thomaz <iagomoraest@gmail.com>
2023-08-25 00:24:09 +0000

    Ajustes

--- exercicio.js
+++ exercicio.js
@@ -1,9 +1,10 @@
-const prompt = require('prompt-sync')();
+var prompt = require('prompt-sync')();
 
-let idade = +prompt("Insira sua idade: ");
+var idade = prompt('Insira sua idade: ');
 
 // Desenvolva abaixo uma solução que respeite a seguintes regras:
 
 /* - Se a idade for maior que 18, exiba no console com o comando console.log('Pode beber álcool'), 
 senão exiba, com o mesmo comando a mensagem 'Não Pode beber álcool';
 */
+console.log(idade);
